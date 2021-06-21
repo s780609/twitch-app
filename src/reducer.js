7ddcc2df453e9eb53d64670bddcfb2edd67a4b6e
s780609 <s780609@gmail.com>
2021-06-21 10:26:58 +0800

    fix: fix docs

--- src/reducer.js
+++ src/reducer.js
@@ -13,11 +13,11 @@ const initialState = {
     clientId: "vr0gbze3cw6zvxavzvjjxokdydm7cj",
     clientSecret: "3x2oiag46esvkqhp99ah13q1cr3wyk",
     accessToken: "",
-    redirect_uri: "http://localhost:3000",
-        //`https://s780609.github.io/twitch-app`,
+    redirect_uri: //"http://localhost:3000",
+        `https://s780609.github.io/twitch-app`,
     parentDomain:
-        "localhost",
-        //`s780609.github.io/twitch-app`
+        //"localhost",
+        `s780609.github.io/twitch-app`
 };
 
 export default (state = initialState, action) => {
