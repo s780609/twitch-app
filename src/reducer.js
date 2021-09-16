e444197a1c3a4bc368165982587b5852d2fa0d76
HsuTingHuan <s780609@gmail.com>
2021-09-16 17:15:03 +0800

    chore: update docs

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 const redirect_uri = 
-"https://hsutinghuan.ddns.net/Web1";
-//`https://s780609.github.io/twitch-app`;
+//"https://hsutinghuan.ddns.net/Web1";
+`https://s780609.github.io/twitch-app`;
 //`http://localhost:3000`;
 
 const port = new URL(redirect_uri).port ? ":" + new URL(redirect_uri).port : "";
