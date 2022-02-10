a6ded78c9dad865a6db54e45fc114a36528df40a
HsuTingHuan <s780609@gmail.com>
2022-02-10 15:46:03 +0800

    chore: modify iframe's width

--- src/components/IFrame.js
+++ src/components/IFrame.js
@@ -2,22 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { Card, CardColumns, Figure } from 'react-bootstrap';
 
 function IFrame({ height, width, allowfullscreen = true, autoplay, videoId, title, description, parent }) {
+    if (width == undefined) width = "400px";
     return (
-        <Card border="light" style={{ width: '400px' }}>
-            {/* <div style={{ width: "400px", margin: "10px", marginRight: "-50px", display: "inline-block" }}> */}
-            <iframe
-                title={title}
-                src={`https://player.twitch.tv/?autoplay=${autoplay == null ? false : true}&parent=${parent}&video=${videoId}`}
-                height={height == null ? 300 : height}
-                width={width == null ? 400 : width}
-                allowfullscreen={allowfullscreen == null ? true : allowfullscreen}
-            ></iframe>
-            {/* </div> */}
-            <Card.Header>{title}</Card.Header>
+        <Card border="light" style={{ maxWidth: width, width: width, display: "BLOCK", margin: "auto" }}>
             <Card.Body>
+                <iframe
+                    title={title}
+                    src={`https://player.twitch.tv/?autoplay=${autoplay == null ? false : true}&parent=${parent}&video=${videoId}`}
+                    height={height == null ? 300 : height}
+                    width={width == null ? 400 : width}
+                    allowfullscreen={allowfullscreen == null ? true : allowfullscreen}
+                ></iframe>
+                <p style={{ maxWidth: width, textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" }}>
+                    {title}
+                </p>
             </Card.Body>
         </Card>
     );
 }
+
+// .wrapper {
+//     padding: 20px;
+//     background: #eaeaea;
+//     max-width: 400px;
+//     margin: 50px auto;
+//   }
+
+// .demo-2 {
+//     overflow: hidden;
+//     white-space: nowrap;
+//     text-overflow: ellipsis;
+//     max-width: 150px;
+//   }
 
 export default IFrame;
